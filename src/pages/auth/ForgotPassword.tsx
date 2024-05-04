3dbe3c908dac5ff39484caea46577c1488d7c306
EstebanSL <gandhisanchezl00@gmail.com>
2024-05-04 15:31:35 -0500

    Recover password page new styles

--- src/pages/auth/ForgotPassword.tsx
+++ src/pages/auth/ForgotPassword.tsx
@@ -1,13 +1,12 @@
-import { Link } from 'react-router-dom';
-import { InputComponent } from '../../components';
-import { ButtonComponent } from '../../components';
-import { forgotPasswordService } from './services/Auth.service';
-import { useFetchAndLoad } from '../../hooks';
-import { useForm } from 'react-hook-form';
-import { errorToast, successToast } from '../../utilities/toasts';
+import { Link } from "react-router-dom";
+import { InputComponent } from "../../components";
+import { ButtonComponent } from "../../components";
+import { forgotPasswordService } from "./services/Auth.service";
+import { useFetchAndLoad } from "../../hooks";
+import { useForm } from "react-hook-form";
+import { errorToast, successToast } from "../../utilities/toasts";
 
 export const ForgotPassword = () => {
-
   const { loading, callEndpoint } = useFetchAndLoad();
 
   const {
@@ -17,7 +16,7 @@ export const ForgotPassword = () => {
   } = useForm();
 
   const onSubmit = async (data: any) => {
-    const { email } = data; 
+    const { email } = data;
 
     try {
       const response = await callEndpoint(
@@ -25,65 +24,78 @@ export const ForgotPassword = () => {
           email,
         })
       );
-      successToast('Recovery password sent successfully')
+      successToast("Recovery password sent successfully");
     } catch (error: any) {
-      errorToast(error.response.data.msg)
+      errorToast(error.response.data.msg);
       console.log(error);
     }
   };
 
   return (
-    <div className="h-screen flex flex-col justify-center w-full max-w-screen-md">
-      <form
-        onSubmit={handleSubmit(onSubmit)}
-        className="bg-white p-4 py-8 flex flex-col gap-2"
-      >
-          <h1 className="text-4xl text-center mb-6 font-bold text-slate-900">
-            FORGOT PASSWORD
-          </h1>
-        <p className="text-center font-semibold text-slate-900 my-4">
-          We'll send you an email with the instructions to reset your password
-        </p>
-        <InputComponent
-          labelText="Email"
-          id="email"
-          type="text"
-          placeholder="Email"
-          register={register}
-          additionalInputStyles={errors.email && 'border-b-red-300'}
-          validationSchema={{
-            required: {
-              value: true,
-              message: 'Email is required',
-            },
-            minLength: {
-              value: 6,
-              message: 'Please enter a minimum of 6 characters',
-            },
-            pattern: {
-              value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-              message: 'invalid email address',
-            },
-          }}
-          errors={errors}
+    <div className="w-full min-h-screen flex gap-10">
+      <div className="w-full m-auto rounded-[50%] hidden lg:flex items-center justify-center bg-gradient-to-r max-w-screen-md p-4">
+        <img
+          src="undraw_forgot_password_re_hxwm.svg"
+          className="w-full max-w-md"
+          alt="login image"
         />
+      </div>
+      <div className="flex flex-col gap-4 justify-center w-full max-w-screen-md">
+        <img
+          src="Google Tasks.svg"
+          width="200"
+          height="200"
+          className="text-center mx-auto max-w-md"
+          alt="login image"
+        />
+        <div className="text-center flex flex-col gap-4">
+          <h2 className="text-4xl">Recover your password</h2>
+          <span className="text-md text-gray-400">
+            We'll send you an email with the instructions to reset your password
+          </span>
+        </div>
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="bg-white p-4 py-8 flex flex-col gap-2"
+        >
+          <InputComponent
+            labelText="Email"
+            id="email"
+            type="text"
+            placeholder="Email"
+            register={register}
+            additionalInputStyles={errors.email && "border-b-red-300"}
+            validationSchema={{
+              required: {
+                value: true,
+                message: "Email is required",
+              },
+              minLength: {
+                value: 6,
+                message: "Please enter a minimum of 6 characters",
+              },
+              pattern: {
+                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                message: "invalid email address",
+              },
+            }}
+            errors={errors}
+          />
 
-        <ButtonComponent
-          loading={loading}
-          type="submit"
-          btnText="Send instructions"
-          addtionalStyles="mt-4"
-          styleType='primary'
-        />
-        <nav className="flex flex-wrap justify-between max-sm:flex-col max-sm:text-center mt-4 text-gray-600 font-normal gap-4">
-          <Link className="hover:underline sm:text-center" to="/">
-            Already have an account? Log In
-          </Link>
-          <Link className="hover:underline  sm:text-center" to="/register">
-            Don't have an account? Register here!
-          </Link>
-        </nav>
-      </form>
+          <ButtonComponent
+            loading={loading}
+            type="submit"
+            btnText="Send instructions"
+            addtionalStyles="mt-4"
+            styleType="primary"
+          />
+          <nav className="flex flex-wrap justify-between max-sm:flex-col max-sm:text-center mt-4 text-gray-600 font-normal gap-4">
+            <Link className="ml-auto text-[15px]" to="/">
+              Back to <span className="text-primary underline">Log In</span>
+            </Link>
+          </nav>
+        </form>
+      </div>
     </div>
   );
 };
